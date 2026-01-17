8db911322c082396eca7dc5df12384e8eff33c19
Vinicius Pinelli <viniciuspinelli@gmail.com>
2026-01-17 13:58:49 -0300

    fff

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+// Servir arquivos HTML sem extensão
+app.get('/setup-admin', (req, res) => {
+  res.sendFile(__dirname + '/public/setup-admin.html');
+});
+
+app.get('/registro', (req, res) => {
+  res.sendFile(__dirname + '/public/registro.html');
+});
+
+app.get('/lab', (req, res) => {
+  res.sendFile(__dirname + '/public/lab.html');
+});
+
+
 // PostgreSQL connection
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
